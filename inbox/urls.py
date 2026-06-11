4f140b08a302726f5a3366a7cae7af861a944863
HeDieh <ohadihedi@gmail.com>
2026-06-11 13:38:50 +0330

    asdf

--- inbox/urls.py
+++ inbox/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from inbox.views import inbox
+from inbox.views import inbox, send_massage
 
 urlpatterns = [
     path('list_messages/<str:username>/<str:password>/', inbox, name='inbox'),
+    path('send_messages/<str:username>/<str:password>/<str:reciver_username>/<str:message>', send_massage, name='send-message'),
 ]
--- inbox/views.py
+++ inbox/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 from inbox.models import inbox_messages
 
 
@@ -17,3 +17,37 @@ def inbox(request, username, password):
     for message in messages:
         txt += f"message: {message.message}, sender: {message.sender}\n"
     return HttpResponse(txt)
+
+def send_massage (request,username,password,reciver_username,message):
+    try:
+        sender_user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return HttpResponse("Invalid username or password")
+
+    if not sender_user.check_password(password):
+        return HttpResponse("Invalid username or password")
+    
+    # body = request.POST
+    # message = body.get("message")
+    # reciever_username = body.get("target_username")
+
+    try:
+        reciever_user = User.objects.get(username=reciver_username)
+    except User.DoesNotExist:
+        return HttpResponse("Invalid reciver_username")
+    
+    create_massage(
+        sender=sender_user,
+        reciver=reciever_user,
+        message=message,
+    )
+
+    return HttpResponse("Message sent successfully")
+
+
+def create_massage(request,sender,massage,reciver):
+    inbox_messages.objects.create(
+        sender=sender,
+        reciver=reciver,
+        massage=massage
+    )
