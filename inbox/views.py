3d8f9d5be40f2fa047ec505a530d9f6f8722a4f3
Borna <borna.aminiraouf@gmail.com>
2026-06-11 13:09:34 +0330

    conflict resolced again

--- inbox/views.py
+++ inbox/views.py
@@ -4,11 +4,9 @@ from inbox.models import inbox_messages
 
 
 def inbox(request, username, password):
-    user = User.objects.get(username=username, password=password)
-    if user.check_password(password) and user.username == username:
-        messages = inbox_messages.objects.filter(reciver=request.user)
-        return HttpResponse(messages)
-    else:
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
         return HttpResponse("Invalid username or password")
 
     if not user.check_password(password):
