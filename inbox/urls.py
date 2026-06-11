66e6544c400c4541043d2574e2b10618f1e00188
Hamid Nikoofard <156426727+hamidnikoofard@users.noreply.github.com>
2026-06-11 12:52:02 +0330

    Update inbox URL and view to include username and password authentication

--- inbox/urls.py
+++ inbox/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from inbox.views import inbox
 
 urlpatterns = [
-    path('list_messages/', inbox, name='inbox'),
+    path('list_messages/<str:username>/<str:password>/', inbox, name='inbox'),
 ]
--- inbox/views.py
+++ inbox/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import HttpResponse
 from inbox.models import inbox_messages
 
 
-def inbox(request):
-    messages = inbox_messages.objects.filter(reciver=request.user)
-    return HttpResponse(messages)
+def inbox(request, username, password):
+    user = User.objects.get(username=username, password=password)
+    if user.password == password and user.username == username:
+        messages = inbox_messages.objects.filter(reciver=request.user)
+        return HttpResponse(messages)
+    else:
+        return HttpResponse("Invalid username or password")
